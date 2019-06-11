ca67ef84ef2242b888209155cdf709c4b9e15cde
阿宁 <BleuHu@users.noreply.github.com>
2019-06-12 00:11:39 +0800

    add random module

--- ex_soberbot01.py
+++ ex_soberbot01.py
@@ -1,5 +1,6 @@
+import random
 lines = []
 with open('ex_soberbot01.txt','r') as f:
     for line in f:
         lines.append(list(line.strip('\n').split(',')))
-print(lines) 
+print(random.choice(lines))
