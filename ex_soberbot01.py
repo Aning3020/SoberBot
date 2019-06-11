c63dceaf72574586f1b3365f0111369caf9953e2
阿宁 <BleuHu@users.noreply.github.com>
2019-06-11 23:56:27 +0800

    update - m 'split every sentence as a date'

--- ex_soberbot01.py
+++ ex_soberbot01.py
@@ -1,3 +1,5 @@
-line = [] #空表格存每行数据
-f = open("ex_soberbot01.txt","r") #读取 .txt
-print(f.read()) #打印 .txt 内容（默认输出全部文件内容）
+lines = []
+with open('ex_soberbot01.txt','r') as f:
+    for line in f:
+        lines.append(list(line.strip('\n').split(',')))
+print(lines) 
