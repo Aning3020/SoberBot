4ea681bd852bca07829cb31a0ac10875e7eb0b0f
阿宁 <BleuHu@users.noreply.github.com>
2019-06-12 20:31:05 +0800

    out of list

--- ex_soberbot01.py
+++ ex_soberbot01.py
@@ -2,5 +2,9 @@ import random
 lines = []
 with open('ex_soberbot01.txt','r') as f:
     for line in f:
-        lines.append(list(line.strip('\n').split(',')))
-print(random.choice(lines))
+        lines.append(line.strip('\n').split(','))
+#print(lines) # txt 装到 list 里
+list_sentence = random.choice(lines) #随机返回一个句子，句子在list里
+#print(list_sentence)
+sentence = random.choice(list_sentence) #脱list
+print(sentence)
